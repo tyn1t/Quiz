7a932b2ef6c1e62413605d8b369fa1d140f344fc
Giovanni <anderlansouza71@gmail.com>
2026-03-10 15:13:36 -0400

    git dominio

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig: import('next').NextConfig = {
+  basePath: '/Quiz',
+  assetPrefix: '/ Quiz/',
+  output: 'export',
+  images: {
+    domains: ["tyn1t.github.io"],
+    unoptimized: true,
+  },
+  // publicRuntimeConfig : {
+  //   imagePath: process.env.NEXT_PUBLIC_IMAGE_PATH || "/perfil",
+  // }
 };
+
 
 export default nextConfig;
